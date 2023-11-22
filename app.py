5e13dc4e18f7c207da749a511eb3243864d63273
Maninder Singh <admin-maninder@163gccspm.gccspm.gc.ca>
2023-11-22 15:36:44 +0000

    adding filter in cloudaudit

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ scc_logs = []
 logger_resource_name = [f"organizations/{org_id}"]
 # howmany days to fetch from
 days_back_admin = 1
-hours_back_cloudaudit = 12
+days_back_cloudaudit = 365
 
 f_name = f"results-{org_name}.json"
 export_output_name = "temp.ndjson"
@@ -98,7 +98,10 @@ logger_export_adminapis_admin = (
 # define logger_export_adminapis_cloudaudit
 logger_export_adminapis_cloudaudit = (
     f'logName="organizations/{org_id}/logs/cloudaudit.googleapis.com%2Factivity"'
-    f' AND timestamp>="{(datetime.now(timezone.utc) - timedelta(days=hours_back_cloudaudit)).strftime("%Y-%m-%dT%H:%M:%S.%f%z")}"'
+    f' AND resource.type="folder"'
+    f' OR resource.type="project"'
+    f' OR resource.type="logging_sink"'
+    f' AND timestamp>="{(datetime.now(timezone.utc) - timedelta(days=days_back_cloudaudit)).strftime("%Y-%m-%dT%H:%M:%S.%f%z")}"'
     f' AND timestamp<"{(datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%f%z"))}"'
 )
 
